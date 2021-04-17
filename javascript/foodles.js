13e9017c0bfa473b4a6f74bfbcc3b2855d4053b4
yashgangwar123 <gangwaryash06@gmail.com>
2021-04-17 14:03:32 +0530

    Update foodles.js

--- javascript/foodles.js
+++ javascript/foodles.js
@@ -1,7 +1,7 @@
 function orderplaced() {
       alert("Order succesfully placed")
       setTimeout(function(){
-          alert("Here is your order:  I LOVE YOU 💖🤗")
+          alert("Here is your order: tumhare baap ke naukar nhi baithe yahan....khud banalo jake 😁")
       },2000)
   }
 
